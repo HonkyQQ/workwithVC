b46fcf05bba9df12fcef8eb55f9ae840f17100a5
Olly <olly.aaa@mail.ru>
2024-04-09 21:38:57 +0500

    task1 lesson3

---
+++ lesson03/task5/Program.cs
@@ -0,0 +1,10 @@
+﻿double CalculeteFormula(int a, int b, int c, int d)
+{
+    double numenator = a * b;
+    int denomenator = c + d;
+    double result = numenator / denomenator;
+    return result;
+}
+
+double result = CalculeteFormula(1, 2, 3, 4);
+Console.WriteLine(result);
