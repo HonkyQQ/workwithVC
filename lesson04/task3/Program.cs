f1048fe48ffab285c285ff4d337dcb0d39eb9b20
Olly <olly.aaa@mail.ru>
2024-04-10 20:47:36 +0500

    task3 lesson4

---
+++ lesson04/task3/Program.cs
@@ -0,0 +1,16 @@
+﻿string GetLetterFromsString(string s)
+{
+    string letters = "";
+    foreach (char e in s)
+    {
+        if (char.IsAsciiLetter(e) == true)
+        {
+            letters = letters + e;
+        }
+    }
+    return letters;
+}
+
+string str = Console.ReadLine();
+string result = GetLetterFromsString(str);
+Console.WriteLine(result);
